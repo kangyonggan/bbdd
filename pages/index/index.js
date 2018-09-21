630cccd3a62dd2a0a5fe1a53c5d13cb1d36f16aa
kangyonggan <kangyonggan@gmail.com>
2018-09-21 18:40:38 +0800

    init

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,38 +5,74 @@ const app = getApp()
 Page({
   data: {
     hasLogin: false,
-    userInfo: {},
+    openId: '',
+    errMsg: '登录中...'
   },
   onLoad: function () {
-    let that = this;
-    wx.getStorage({
-      key: 'userInfo',
-      success: function (res) {
-        if (res && res.data) {
-          that.setData({
-            hasLogin: true,
-            userInfo: res.data
-          })
-          wx.setNavigationBarTitle({
-            title: res.data.nickName
-          })
-        }
-      }
-    });
+    this.login();
   },
-  getUserInfo: function(e) {
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    // 显示顶部刷新图标
+    wx.showNavigationBarLoading();
     this.setData({
-      hasLogin: true,
-      userInfo: e.detail.userInfo
-    })
-
-    wx.setNavigationBarTitle({
-      title: e.detail.userInfo.nickName
-    })
-
-    wx.setStorage({
-      key: 'userInfo',
-      data: e.detail.userInfo
+      errMsg: '登录中...'
     });
+    this.login();
+  },
+  login: function() {
+    let that = this;
+    wx.login({
+      success: function (res) {
+        wx.request({
+          url: 'https://api.weixin.qq.com/sns/jscode2session',
+          data: {
+            appid: 'wx94338920deda7272',
+            secret: '15e3441a7b41d18d344edbb4df2822fa',
+            js_code: res.code,
+            grant_type: 'authorization_code'
+          },
+          method: 'GET',
+          success: function (res) {
+            app.openId = res.data.openid;
+            that.setData({
+              hasLogin: true,
+              openid: res.data.openid,
+              errMsg: '登录成功'
+            })
+
+            // 隐藏导航栏加载框
+            wx.hideNavigationBarLoading();
+            // 停止下拉动作
+            wx.stopPullDownRefresh();
+          },
+          fail: function () {
+            that.setData({
+              hasLogin: false,
+              errMsg: '登录失败，下拉重试！'
+            })
+
+            // 隐藏导航栏加载框
+            wx.hideNavigationBarLoading();
+            // 停止下拉动作
+            wx.stopPullDownRefresh();
+          }
+        })
+      },
+      fail: function () {
+        that.setData({
+          hasLogin: false,
+          errMsg: '登录失败，下拉重试！'
+        })
+
+        // 隐藏导航栏加载框
+        wx.hideNavigationBarLoading();
+        // 停止下拉动作
+        wx.stopPullDownRefresh();
+      }
+    })
   }
 })
